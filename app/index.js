52f44f56e3d6fba06374c750df5bb11a787a5925
Irapfaya <arindomudiare@gmail.com>
2021-07-13 23:32:08 +0100

    setup webpack and babel config

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Finally React Agbasa!!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
