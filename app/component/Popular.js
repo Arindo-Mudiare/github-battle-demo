d8ba730d21e9bb65696756658b2278b2de8f40a6
Irapfaya <arindomudiare@gmail.com>
2021-07-14 22:51:06 +0100

    add minimal styling and selecting Functionality to Nav

--- app/component/Popular.js
+++ app/component/Popular.js
@@ -1,12 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
 
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+  return (
+    <ul className="flex-center">
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            className="btn-clear nav-link"
+            style={language === selected ? { color: "rgb(187, 46, 31)" } : null}
+            onClick={() => onUpdateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
 export default class Popular extends Component {
-    render() {
-        const languages = ['All', 'Javascript', 'Ruby','Java','CSS','Python']
-        return (
-            <ul className='flex-container'>
-               POPULAR 
-            </ul>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All",
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({ selectedLanguage });
+  }
+  render() {
+    const { selectedLanguage } = this.state;
+
+    return (
+      <Fragment>
+        <LanguagesNav
+          selected={this.selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+      </Fragment>
+    );
+  }
 }
