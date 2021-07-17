ffd3520413627106945b84cc69a7235b8584228f
Irapfaya <arindomudiare@gmail.com>
2021-07-17 19:06:13 +0100

    created functionality for Battle component

--- app/index.js
+++ app/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Battle from "./component/Battle";
 import Popular from "./component/Popular";
 import "./index.css";
 
 export default class App extends Component {
-  
   render() {
     return (
-      <div className='container'>
-        <Popular />
+      <div className="container">
+        <Battle />
       </div>
     );
   }
