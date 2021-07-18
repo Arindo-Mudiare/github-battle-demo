0ee1e23b86fb57ccb26ec87da4d34ee7506f918b
Irapfaya <arindomudiare@gmail.com>
2021-07-18 22:34:02 +0100

    extracted card logic into a dedicated component

--- app/component/Popular.js
+++ app/component/Popular.js
@@ -1,7 +1,13 @@
 import React, { Component, Fragment } from "react";
+import Card from "./Card";
 import { PropTypes } from "prop-types";
 import { fetchPopularRepos } from "../utils/api";
-import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
+import {
+  FaUser,
+  FaStar,
+  FaCodeBranch,
+  FaExclamationTriangle,
+} from "react-icons/fa";
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
   const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
@@ -29,49 +35,44 @@ LanguagesNav.propTypes = {
 
 function ReposGrid({ repos }) {
   return (
-    <ul className='grid space-around'>
+    <ul className="grid space-around">
       {repos.map((repo, index) => {
-        const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
-        const { login, avatar_url } = owner
+        const { name, owner, html_url, stargazers_count, forks, open_issues } =
+          repo;
+        const { login, avatar_url } = owner;
 
         return (
-          <li key={html_url} className='repo bg-light'>
-            <h4 className="header-lg center-text">
-              #{index + 1}
-            </h4> 
-            <img 
-            className='avatar' 
-            src={avatar_url} 
-            alt={`Avatar for ${login}`} 
-            /> 
-            <h2 className='center-text'>
-              <a className='link' href={html_url}>{login}</a>
-            </h2>
-            <ul className="card-list">
-              <li>
-                <FaUser color='rgba(255,191,116)' size={22} />
-                <a href={`https://github.com/${login}`}>{login}</a>
-              </li>
-              <li>
-                <FaStar color='rgba(255,215,0)' size={22} />
-                {stargazers_count.toLocaleString()}stars
-              </li>
-              <li>
-                <FaCodeBranch color='rgba(255,195,245)' size={22} />
-                {forks.toLocaleString()}forks
-              </li>
-              <li>
-                <FaStar color='rgba(241,138,147)' size={22} />
-                {open_issues.toLocaleString()} open issues
-              </li>
-              
-            </ul>
+          <li key={html_url}>
+            <Card
+              header={`#${index + 1}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
+            >
+              <ul className="card-list">
+                <li>
+                  <FaUser color="rgba(255,191,116)" size={22} />
+                  <a href={`https://github.com/${login}`}>{login}</a>
+                </li>
+                <li>
+                  <FaStar color="rgba(255,215,0)" size={22} />
+                  {stargazers_count.toLocaleString()}stars
+                </li>
+                <li>
+                  <FaCodeBranch color="rgba(255,195,245)" size={22} />
+                  {forks.toLocaleString()}forks
+                </li>
+                <li>
+                  <FaStar color="rgba(241,138,147)" size={22} />
+                  {open_issues.toLocaleString()} open issues
+                </li>
+              </ul>
+            </Card>
           </li>
-        )
+        );
       })}
-      
     </ul>
-  )
+  );
 }
 export default class Popular extends Component {
   constructor(props) {
@@ -97,30 +98,29 @@ export default class Popular extends Component {
     });
 
     // make a fetch request if there is no data
-    if (!this.state.repos[selectedLanguage]) { 
-
+    if (!this.state.repos[selectedLanguage]) {
       fetchPopularRepos(selectedLanguage)
-      .then((data) => {
-        this.setState(({ repos }) => ({
-          repos: {
-            ...repos,
-            [selectedLanguage]: data
-          }
-        }))
-      })
+        .then((data) => {
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data,
+            },
+          }));
+        })
         .catch(() => {
           console.warn("Error fetching repos: ", error);
-  
+
           this.setState({
             error: `There was an error fetching the repositories`,
-          })
-        })
+          });
+        });
     }
   }
 
   isLoading() {
-    const { selectedLanguage, repos, error } = this.state
-    return !repos[selectedLanguage] && error === null
+    const { selectedLanguage, repos, error } = this.state;
+    return !repos[selectedLanguage] && error === null;
   }
   render() {
     const { selectedLanguage, repos, error } = this.state;
@@ -136,7 +136,9 @@ export default class Popular extends Component {
 
         {error && <p>{error}</p>}
 
-        {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]}/> }
+        {repos[selectedLanguage] && (
+          <ReposGrid repos={repos[selectedLanguage]} />
+        )}
       </Fragment>
     );
   }
